d1bcba7b94bc774068ec49c2e55c7e9e0d1a178d
Hasan <tuhin@domaingood.com>
2024-05-13 09:33:42 +0600

    first commit

--- PHP Variables/variables.php
+++ PHP Variables/variables.php
@@ -6,11 +6,10 @@
 </head>
 <body>
 <?php $name = "Aisle Nevertell";
-$favorite_band = "I Panjohur";
+$favorite_band = "Apple Inc.";
 
-echo "Heya I'm $name and my favorite band is $favorite_band";
+echo "Hey I'm $name and my favorite band is $favorite_band";
 ?>
 
-1.This PHP code includes both single and multi line comments. Take a moment to predict what will show up in the terminal and what will not. When you’re ready run the code to see if you were right.
 </body>
 </html>
---
+++ phpinfo.php
@@ -0,0 +1,28 @@
+<?php
+phpinfo();
+?>
+/*
+ *   The above code will display all the PHP configuration information.
+Step 5:  Save the file and access it through the web browser.
+http://localhost/phpinfo.php
+
+The above URL will show you all the PHP configuration details.
+That’s it. You have successfully installed PHP on your Windows system.
+Also Read:  How to Install PHP on Ubuntu 18.04
+Conclusion
+Now you have learned how to install PHP on Windows 10. If you have any queries regarding this then please don’t hesitate to ask.
+Share this: Tweet Share on Tumblr Email Email Print Print
+
+Related
+
+Post was not sent - check your email addresses!
+
+Email check failed, please try again
+
+Sorry, yourPOSTwas not sent - check your email addresses!
+
+Email check failed, please try again
+
+Sorry, your blog cannot share posts by email.
+ */
+
