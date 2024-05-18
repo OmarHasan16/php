d144dc1929367f7258681278a556fa5a6edaff62
Hasan <tuhin@domaingood.com>
2024-05-18 07:21:04 +0600

    first commit

--- PHP Variables/variables.php
+++ PHP Variables/variables.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Variables</title>
-</head>
-<body>
-<?php $name = "Aisle Nevertell";
+<?php
+// Write your code
+
+$name = "Aisle Nevertell";
 $favorite_band = "Apple Inc.";
 
 echo "Hey I'm $name and my favorite band is $favorite_band";
-?>
-
-</body>
-</html>
